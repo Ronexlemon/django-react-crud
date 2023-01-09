786bddcc453baef382f9af2459d99f6b7f4f0bea
RonexLemon <ronexondimu@gmail.com>
2023-01-09 07:56:01 +0300

    implemented post to server function

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { useState } from "react";
 import TopBar from "./topbar";
+import axios from "axios";
 
 const Create= ()=>{
     const [title,setTitle] = useState();
     const [description,setDescription] = useState("");
     const [completed,setCompleted] = useState(false);
+    async function postTodo(title, description, completed) {
+        try {
+          const response = await axios.post('http://127.0.0.1:8000/api/todos/', {
+            title,
+            description,
+            completed
+          });
+          console.log(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      const handleOnSubmit =async()=>{
+       await  postTodo(title,description,completed);
+       alert("added");
+        
+      }
     
     return(
         <div className=" h-screen w-full bg-black ">
@@ -14,7 +32,7 @@ const Create= ()=>{
 
             
             <div  className="bg-slate-400 w-1/2 h-1/2 rounded-2xl  grid grid-rows-4 gap-4  justify-center">
-                <form>
+                <form onSubmit={()=> handleOnSubmit()}>
                     <label>Title:</label><br/>
                     <input className="text-center"  type="text" placeholder="The great man "  onChange={event=>setTitle(event.target.value)}/><br/>
                     <label>Description:</label><br/>
@@ -28,11 +46,13 @@ const Create= ()=>{
                             </div>: <div>
                         <label>False</label><br/>
                     <input type="checkbox"  placeholder="The great man  "onClick={()=>{setCompleted(!true)}} />
-                        </div>}
+                        </div>}<br/>
+                        <button type="submit" >Submit</button>
                         
                        
                    
                     </div>
+
                 </form>
                    
                     
--- src/components/display.jsx
+++ src/components/display.jsx
@@ -34,6 +34,7 @@ const Display =()=>{
           console.error("yoolw");
         }
       };
+      
       useEffect(()=>{
         fetchTodos();
       })
